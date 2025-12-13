92fc271bd3ab50959b53ab87a28ddeaf4ba0305f
Knight~ <x665adex563@gmail.com>
2025-12-13 11:28:57 +0800

    可更改購物車內商品數量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,4 +8,17 @@ class CartItemsController < ApplicationController
     flash[:warning] = "成功將 #{@product.title} 從購物車刪除！"
     redirect_to :back
   end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_items).permit(:quantity)
+  end
 end
