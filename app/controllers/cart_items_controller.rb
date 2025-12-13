d7716ab3b4ac222ff595e006f7c6394cf58865bf
Knight~ <x665adex563@gmail.com>
2025-12-13 11:40:48 +0800

    在購物車更新數量時 不能超過庫存

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,12 +13,19 @@ class CartItemsController < ApplicationController
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
 
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足以加入購物車"
+    end
+
     redirect_to carts_path
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_items).permit(:quantity)
+    params.require(:cart_item).permit(:quantity)
   end
 end
