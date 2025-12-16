9818e923ae1ef45f9e215f02ca64b598c9c39b32
Knight~ <x665adex563@gmail.com>
2025-12-16 14:52:39 +0800

    feat(order): 規範訂單狀態轉換

--- app/models/order.rb
+++ app/models/order.rb
@@ -29,5 +29,25 @@ class Order < ApplicationRecord
     state :shipped
     state :order_cancelled
     state :good_returned
+
+    event :make_payment do
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid,         to: :shipping
+    end
+
+    event :deliver do
+      transitions from: :shipping,     to: :shipped
+    end
+
+    event :return_good do
+      transitions from: :shipped,      to: :good_returned
+    end
+
+    event :cancel_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+    end
   end
 end
